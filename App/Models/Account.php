c2189906a93b3205f417e097a7bbe22f75b0bae0
BambusControl <mojmir.majer@gmail.com>
2020-11-23 23:52:10 +0100

    Účty sa už dajú odstrániť, mierny refaktoring kódu a úprava vzhľadu.

--- App/Models/Account.php
+++ App/Models/Account.php
@@ -17,20 +17,21 @@ class Account extends Model
     protected $lastname;
     protected $date_of_birth;
     protected $gender;
-    protected $created_at;
 
     /**
      * Account constructor.
-     * @param $username
-     * @param $email
-     * @param $password
-     * @param $firstname
-     * @param $lastname
-     * @param $date_of_birth
-     * @param $gender
+     * @param $id
+     * @param string $username
+     * @param string $email
+     * @param string $password
+     * @param string $firstname
+     * @param string $lastname
+     * @param string $date_of_birth
+     * @param string $gender
      */
-    public function __construct($username = null, $email = null, $password = null, $firstname = null, $lastname = null, $date_of_birth = null, $gender = null)
+    public function __construct($id = null, $username = '', $email = '', $password = '', $firstname = '', $lastname = '', $date_of_birth = '', $gender = '')
     {
+        $this->id = $id;
         $this->username = $username;
         $this->email = $email;
         $this->password = $password;
@@ -50,8 +51,7 @@ class Account extends Model
             'firstname',
             'lastname',
             'date_of_birth',
-            'gender',
-            'created_at'
+            'gender'
         ];
     }
 
@@ -61,17 +61,17 @@ class Account extends Model
     }
 
     /**
-     * @return mixed
+     * @return int|null
      */
-    public function getCreatedAt()
+    public function getId() : ?int
     {
-        return $this->created_at;
+        return $this->id;
     }
 
     /**
      * @return string
      */
-    public function getUsername() : string
+    public function getUsername(): string
     {
         return $this->username;
     }
@@ -79,9 +79,62 @@ class Account extends Model
     /**
      * @return string
      */
-    public function getEmail() : string
+    public function getEmail(): string
     {
         return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateOfBirth(): string
+    {
+        return $this->date_of_birth;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGender(): string
+    {
+        return $this->gender;
+    }
+
+    public function as_array()
+    {
+        $array_v = array_reverse( array_values((array) $this), false );
+        $array = array();
+
+        foreach (self::setDbColumns() as $key)
+        {
+            $array[$key] = array_pop($array_v);
+        }
+
+        return $array;
     }
 
 }
